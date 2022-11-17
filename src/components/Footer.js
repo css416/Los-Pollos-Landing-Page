372b662acbe91e7daba2a4ff3271cd29a8d4f988
Rondle Luke Pasumbal <ronpasumbal@gmail.com>
2022-11-18 04:23:27 +0800

    refactored footer for better screen responsiveness

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,62 +8,86 @@ const Footer = () => {
     <footer>
       <div className="footer-container">
         <div className="footer-content">
-          <div className="footer-details">
-            <div className="footer-image">
-              {/* <Link to="/" className="logo"><img src={square_logo} alt="" /></Link> */}
+
+            {/* <div className="footer-image">
               <a href="/">
                 <img src={square_logo} alt="" />
               </a>
-            </div>
-            <p>
-              Monday to Thursday (7 AM - 10 PM) <br />
-              Friday to Saturday (7 AM - 12 AM) <br />
-              (Drive-thru open until 1 AM Friday) <br />
-              Sunday (7 AM - 9 PM)
-            </p>
-          </div>
+            </div> */}
 
-          <div className="footer-contact">
-            <h2>Contact Info</h2>
-            <div className="footer-contact-details">
-              <address>
-                12000 - 12100 Coors Rd SW, Albuquerque, New Mexico 87045
-              </address>
-              {/* <Link to="mailto:customercare@lph.com">customercare@lph.com</Link> */}
-              <span>customercare@lph.com</span>
-              <span>(505) 146-0195</span>
-            </div>
-          </div>
-
-          <div className="quick-links">
-            <h2>Quick Links</h2>
-            <div className="quick-links-container">
-              <a href="#header-video">Home</a>
-              <a href="#about">About</a>
-              <a href="#menu">Menu</a>
-              <a href="#gallery">Gallery</a>
-              <a href="#contact">Contact Us</a>
-            </div>
-          </div>
-
-          <div className="subscription">
-            <h2>Subscription</h2>
-            <span>Don't forget to subscribe to our newsletter</span>
-            {/* <form method="POST" className="submit-email">
-              <div className="input-container">
-                <input type="text" name="email" placeholder="Email" />
-                <div className="button"><p><FontAwesomeIcon icon={faPaperPlane} /></p></div>
+          {/* <div className="footer-info"> */}
+            <div className="footer-section">
+              <div className="footer-image">
+                <a href="/">
+                  <img src={square_logo} alt="" />
+                </a>
               </div>
-            </form> */}
-            <form className="subscribe_form">
-              <div className="input-group">
-                <input type="text" className="form-control" name="email" placeholder="Enter your email" />
-                <span className="input-group-btn">
-                      <button className="btn btn-default" type="button">subscribe</button>
-                </span>
+            </div>
+
+            <div className="footer-section">
+              {/* <div className="footer-image">
+                <a href="/">
+                  <img src={square_logo} alt="" />
+                </a>
+              </div> */}
+              <h2>Restaurant Hours</h2>
+              <p className="schedule">
+                Monday to Thursday (7 AM - 10 PM) <br />
+                Friday to Saturday (7 AM - 12 AM) <br />
+                (Drive-thru open until 1 AM Friday) <br />
+                Sunday (7 AM - 9 PM)
+              </p>
+            </div>
+
+            <div className="footer-section">
+              <h2>Contact Info</h2>
+              <div className="footer-contact-details">
+                <address>
+                  12000 - 12100 Coors Rd SW, Albuquerque, New Mexico 87045
+                </address>
+                {/* <Link to="mailto:customercare@lph.com">customercare@lph.com</Link> */}
+                <span>customercare@lph.com</span>
+                <span>(505) 146-0195</span>
               </div>
-            </form>
-          </div>
+            </div>
+
+            <div className="footer-section">
+              <h2>Quick Links</h2>
+              <div className="quick-links-container">
+                <ul>
+                  <li><a href="#header-video">Home</a></li>
+                  <li><a href="#about">About</a></li>
+                  <li><a href="#menu">Menu</a></li>
+                  <li><a href="#gallery">Gallery</a></li>
+                  <li><a href="#contact">Contact Us</a></li>
+                </ul>
+                
+              </div>
+            </div>
+
+            <div className="footer-section">
+              <h2>Subscription</h2>
+              <span>Don't forget to subscribe to our newsletter</span>
+              {/* <form method="POST" className="submit-email">
+                <div className="input-container">
+                  <input type="text" name="email" placeholder="Email" />
+                  <div className="button"><p><FontAwesomeIcon icon={faPaperPlane} /></p></div>
+                </div>
+              </form> */}
+              {/* <form className="subscribe_form">
+                <div className="input-group">
+                  <input type="text" className="form-control" name="email" placeholder="Enter your email" />
+                  <span className="input-group-btn">
+                        <button className="btn btn-default" type="button">subscribe</button>
+                  </span>
+                </div>
+              </form> */}
+              <form action="#">
+                <input type="text" />
+                <button>Subscribe</button>  
+              </form>
+            </div>
+          {/* </div> */}
           
         </div>
       </div>
